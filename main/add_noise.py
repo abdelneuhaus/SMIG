f7c1a5a50f8f6144bd206dfadbd38f2a4598698f
abdelneuhaus <abdelghani.neuhaus@u-bordeaux.fr>
2022-04-05 14:14:53 +0200

    background cant be negative

--- main/add_noise.py
+++ main/add_noise.py
@@ -8,4 +8,5 @@ def add_noise(image_to_noised, bckg=498, sd=12):
     poisson = np.random.poisson(image_to_noised, size=image_to_noised.shape)
     gaussian += np.array(poisson, dtype='uint16')
     gaussian[gaussian < 0] = bckg
+    gaussian[gaussian > image_to_noised+bckg+sd] = bckg
     return np.array(gaussian, dtype='uint16')
