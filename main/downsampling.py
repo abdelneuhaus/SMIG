5b913e7b5c7cb2293b704fc5d8d10809d11fda1a
abdelneuhaus <abdelghani.neuhaus@u-bordeaux.fr>
2022-04-25 15:31:28 +0200

    improve simulation speed

--- main/downsampling.py
+++ main/downsampling.py
@@ -1,6 +1,8 @@
 import xarray as xr
 import numpy as np
 
+
 def downsampling(big_image, downsampling_factor=5):
-    downsampled_image = xr.DataArray(big_image, dims=['x', 'y']).coarsen(x=downsampling_factor, y=downsampling_factor).sum()
-    return np.array(downsampled_image.values, dtype='uint16')
+    b = big_image.shape[0]//downsampling_factor
+    downsampled_image = big_image.reshape(-1, downsampling_factor, b, downsampling_factor).sum((-1, -3))
+    return np.array(downsampled_image, dtype='uint16')
