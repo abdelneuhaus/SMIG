b67fe2c76c1ef3f4bcf057bd0fb87104ff641762
abdelneuhaus <abdelghani.neuhaus@u-bordeaux.fr>
2022-06-02 13:45:47 +0200

    points can be genered as a circle

--- main/create_molecules_data.py
+++ main/create_molecules_data.py
@@ -28,7 +28,7 @@ def create_molecules_data(frames, nbr_molecules=20, size_image=2500, randomize=T
                 data[i] = {'coordinates': generate_grid_coordinates(size_image, edge), 'intensity': generate_intensity(value, ii_sd), 'on_times': generate_on_times(frames, randomize=randomize, off_length_min=off_length_min, off_length_max=off_length_max, number_blink_min=number_blink_min, number_blink_max=number_blink_max)}
             else:
                 data[i] = {'coordinates': generate_coordinates(size_image, edge), 'intensity': generate_intensity(value, ii_sd), 'on_times': own_blink}
-    for j in range(nbr_molecules, int(nbr_molecules/3)):
+    for j in range(nbr_molecules, nbr_molecules+20):
         if (grid == True) or (circle == True):
-            data[j] = {'coordinates': generate_coordinates(size_image, edge), 'intensity': generate_intensity(value/2, ii_sd/2), 'on_times': generate_on_times(frames, randomize=randomize, off_length_min=frames-1, off_length_max=frames-1, number_blink_min=0, number_blink_max=1, beads=True)}
+            data[j] = {'coordinates': generate_coordinates(size_image, edge), 'intensity': generate_intensity(value/2, value/10), 'on_times': generate_on_times(frames, randomize=randomize, off_length_min=frames-1, off_length_max=frames-1, number_blink_min=0, number_blink_max=1, beads=True)}
     return data
--- main/generate_stack.py
+++ main/generate_stack.py
@@ -20,16 +20,18 @@ def generate_stack(frames, nb_emitters, filename, randomize=True, intensity=6000
                                    off_length_min=length_min, off_length_max=length_max, 
                                    number_blink_min=blink_min, number_blink_max=blink_max, own_blink=blinking_seq, 
                                    edge=edge, grid=grid, circle=circle, num_circle=num_circle)
+    if save == False:
+        data = generate_one_frame(points, y_image, frame=0)
+        gaussian_image = gaussian_filter(data, sigma=5)
+        down_image = downsampling(gaussian_image)
+        out = add_noise(down_image, bckg=background_value, sd=sd_bckg_value)
+        return out
     with tifffile.TiffWriter(filename) as tif:
         for i in range(frames):
             data = generate_one_frame(points, y_image, frame=i)
             gaussian_image = gaussian_filter(data, sigma=5)
             down_image = downsampling(gaussian_image)
             out = add_noise(down_image, bckg=background_value, sd=sd_bckg_value)
-            if save==True:
-                tif.write(out, photometric='minisblack')
-                save_data(points, x_image, filename)
-            if save==False:
-                return out
-        if save==True:
-            save_parameters(filename, frames, nb_emitters, intensity, length_min, length_max, blink_min, blink_max, background_value, sd_bckg_value, blinking_seq, edge)
+            tif.write(out, photometric='minisblack')
+            save_data(points, x_image, filename)
+        save_parameters(filename, frames, nb_emitters, intensity, length_min, length_max, blink_min, blink_max, background_value, sd_bckg_value, blinking_seq, edge)
