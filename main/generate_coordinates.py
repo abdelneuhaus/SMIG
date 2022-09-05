4b8b4a09a87edf4cee16759cb9ded231f52f8852
abdelneuhaus <abdelghani.neuhaus@u-bordeaux.fr>
2022-09-05 17:29:32 +0200

    diffusion data now correctly saved

--- main/generate_coordinates.py
+++ main/generate_coordinates.py
@@ -2,5 +2,4 @@ import random
 
 
 def generate_coordinates(size_image=500, edge=0):
-    return [round(random.uniform(edge, size_image-edge), 3), round(random.uniform(edge, size_image-edge), 3)]
-    
+    return [random.uniform(edge, size_image-edge), random.uniform(edge, size_image-edge)]
